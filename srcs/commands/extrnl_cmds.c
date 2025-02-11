f49d970aa05b6630c95a0307a2cb912119b1b787
Rohan Dalal <rdalal@made-f0Br5s12.clusters.42paris.fr>
2025-02-11 19:11:51 +0100

    fixed

--- srcs/commands/extrnl_cmds.c
+++ srcs/commands/extrnl_cmds.c
@@ -6,7 +6,7 @@
 /*   By: rdalal <rdalal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 17:31:21 by rdalal            #+#    #+#             */
-/*   Updated: 2025/02/11 16:37:56 by rdalal           ###   ########.fr       */
+/*   Updated: 2025/02/11 19:11:36 by rdalal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,50 +78,4 @@ void	exec_external(t_token *tokens, char **envp)
 	if (!argv)
 		return ;
 	run_cmd(cmd_path, argv, envp);
-=======
-char	*ft_get_envp(char **envp, const char *name)
-{
-	int	i;
-	int	len;
-
-	i = 0;
-	len = ft_strlen(name);
-	while (envp[i])
-	{
-		if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=')
-			return (envp[i] + len + 1);
-		i++;
-	}
-	return (NULL);
-}
-
-char	*make_path(char *path, char *cmd)
-{
-	char	*full_path;
-	char	*temp;
-
-	temp = ft_strjoin(path, "/");
-	full_path = ft_strjoin(temp, cmd);
-	free (temp);
-	return (full_path);
-}
-
-char	*get_cmd_path(char *cmd, char **envp)
-{
-	int		i;
-	char	*path_env;
-	char	**paths;
-	char	*cmd_path;
-
-	path_env = ft_getenvp(envp, "PATH");
-	if (!path_env)
-		return (NULL);
-	paths = ft_split(path_env, ':');
-	i = -1;
-	while (paths && paths[++i] && cmd)
-	{
-		cmd_path = make_path(paths[i], cmd);
-		if (access(cmd_path, X_OK) == 0)
-		
-	}
 }
