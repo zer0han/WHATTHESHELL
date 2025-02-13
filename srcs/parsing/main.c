8706fc94053b162c2ecf2cceb79d068ee2e6a81f
Gabriel Mechaly <gmechaly@made-f0Ar7s1.clusters.42paris.fr>
2025-02-13 18:45:51 +0100

    fixed segfault with sqt and dqt

--- srcs/parsing/main.c
+++ srcs/parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: gmechaly <gmechaly@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 15:27:16 by gmechaly          #+#    #+#             */
-/*   Updated: 2025/02/13 15:24:05 by gmechaly         ###   ########.fr       */
+/*   Updated: 2025/02/13 18:42:03 by gmechaly         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,41 +35,37 @@ t_token	*ft_minishell_parsing(char *input)
 
 	new_input = expand_variables(input);
 	free(input);
+	if (new_input == NULL)
+		return (NULL);
 	tokens = ft_tokenize(new_input);
-	free_tokens(tokens);
 	free(new_input);
-	if (tokens == NULL || parse_tokens(&tokens) == NULL)
+	if (!tokens || !parse_tokens(&tokens) || !is_null_token(&tokens))
 		return (NULL);
 	return (tokens);
 }
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	char	*input;
-	char	*ninput;
 	t_token	*tokens;
 
+	(void)argc;
+	(void)argv;
+	(void)envp; //temporary
 	input = readline("Enter command: ");
 	tokens = NULL;
-	ninput = NULL;
 	while (input != NULL)
 	{
 		if (*input)
 			add_history(input);
-		ninput = expand_variables(input);
-		if (ninput == NULL)
-			return (free(input), 0);
-		free(input);
-		tokens = ft_tokenize(ninput);
-		if (tokens == NULL || parse_tokens(&tokens) == NULL)
-			return (free_tokens(tokens), free(ninput), 0);
-		ft_print_tokens(&tokens);
-		free_tokens(tokens);
-		free(ninput);
+		tokens = ft_minishell_parsing(input);
+		if (tokens)
+		{
+			ft_print_tokens(&tokens);
+			free_tokens(tokens);
+		}
 		input = readline("Enter command: ");
 	}
 	free(input);
 	return (0);
 }
-
-/*< infile cat | wc -l -c > outfile -> segfault*/
