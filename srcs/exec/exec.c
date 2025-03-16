5edbbea37adb2c1d85ebb5c3385e39057521ca5d
Rohan Dalal <rdalal@bess-f2r4s9.clusters.42paris.fr>
2025-03-16 15:54:01 +0100

    put aside for now

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: rdalal <rdalal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 19:35:01 by rdalal            #+#    #+#             */
-/*   Updated: 2025/03/12 22:26:03 by gmechaly         ###   ########.fr       */
+/*   Updated: 2025/03/16 15:35:15 by rdalal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,6 +114,7 @@ t_exec	*main_execution(t_token **token_tree, char **envp, t_envp *env)
 	t_exec	*exec_list;
 	t_token	*temp;
 	pid_t	pid;
+	t_exec	*current;
 	
 	pid = 0;
 	if (!token_tree || !*token_tree)
@@ -122,22 +123,19 @@ t_exec	*main_execution(t_token **token_tree, char **envp, t_envp *env)
 	exec_list = create_exec_list(temp, envp, env);
 	if (!exec_list)
 		return (NULL);
-	if (exec_list->redir)
-		redirection_process(exec_list->redir);
+	current = exec_list;
+	while (current)
+	{
+		if (current->redir)
+			redirection_process(current, current->redir, envp);
+		current = current->next;
+	}
 	if (exec_list->next)
 		exec_pipeline(exec_list, envp);
 	else if (exec_list->cmd_token->input)
 		execute_cmds(exec_list->cmd_token, envp, exec_list);
 	else
-	{
-		//pid = fork();
-		// if (pid == -1)
-		// 	return (perror("fork"), free_exec(exec_list), NULL);
-		// if (pid == 0)
 		exec_external(exec_list->cmd_token, envp);
-		// else 
-		// 	wait_for_children(exec_list);
-	}
 	return (exec_list);
 }
 
--- srcs/pipe/pipe_helper.c
+++ srcs/pipe/pipe_helper.c
@@ -6,34 +6,84 @@
 /*   By: rdalal <rdalal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 15:45:14 by rdalal            #+#    #+#             */
-/*   Updated: 2025/03/11 21:50:06 by rdalal           ###   ########.fr       */
+/*   Updated: 2025/03/15 21:01:37 by rdalal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// void	handle_pipe_redir(t_exec *exec)
+// {
+// 	if (exec->fd_in != STDIN_FILENO)
+// 		dup2(exec->fd_in, STDIN_FILENO);
+// 	else if (exec->p_pipe >= 0)
+// 		dup2(exec->p_pipe, STDIN_FILENO);
+// 	if (exec->fd_out != STDOUT_FILENO)
+// 		dup2(exec->fd_out, STDOUT_FILENO);
+// 	else if (exec->next)
+// 		dup2(exec->fd_pipe[1], STDOUT_FILENO);
+// 	if (exec->p_pipe >= 0)
+// 		close(exec->p_pipe);
+// 	if (exec->next && exec->fd_pipe[1] >= 0)
+// 	{
+// 		close(exec->fd_pipe[1]);
+// 		exec->fd_pipe[1] = -1;
+// 	}
+// 	if (exec->next && exec->fd_pipe[0] >= 0)
+// 		close(exec->fd_pipe[0]);
+// }
+
 void	handle_pipe_redir(t_exec *exec)
 {
+	printf("[DEBUG] Handling pipe redirection for cmd: %s\n", exec->cmd);
+
 	if (exec->fd_in != STDIN_FILENO)
-		dup2(exec->fd_in, STDIN_FILENO);
-	else if (exec->p_pipe >= 0)
-		dup2(exec->p_pipe, STDIN_FILENO);
-	if (exec->fd_out != STDOUT_FILENO)
-		dup2(exec->fd_out, STDOUT_FILENO);
-	else if (exec->next)
-		dup2(exec->fd_pipe[1], STDOUT_FILENO);
-	if (exec->p_pipe >= 0)
-		close(exec->p_pipe);
-	if (exec->next && exec->fd_pipe[1] >= 0)
 	{
-		close(exec->fd_pipe[1]);
-		exec->fd_pipe[1] = -1;
+		printf("[DEBUG] Redirecting stdin to %d\n", exec->fd_in);
+		if (dup2(exec->fd_in, STDIN_FILENO) == -1)
+		{
+			perror("[ERROR] dup2 failed for stdin");
+			exit(1);
+		}
+		close(exec->fd_in);
 	}
-	if (exec->next && exec->fd_pipe[0] >= 0)
-		close(exec->fd_pipe[0]);
+
+	if (exec->p_pipe >= 0)
+	{
+		printf("[DEBUG] Redirecting stdin from previous pipe %d\n", exec->p_pipe);
+		if (dup2(exec->p_pipe, STDIN_FILENO) == -1)
+		{
+			perror("[ERROR] dup2 failed for pipe input");
+			exit(1);
+		}
+		close(exec->p_pipe);
+	}
+
+	if (exec->fd_out != STDOUT_FILENO)
+	{
+		printf("[DEBUG] Redirecting stdout to %d\n", exec->fd_out);
+		if (dup2(exec->fd_out, STDOUT_FILENO) == -1)
+		{
+			perror("[ERROR] dup2 failed for stdout");
+			exit(1);
+		}
+		close(exec->fd_out);
+	}
+
+	if (exec->next)
+	{
+		printf("[DEBUG] Redirecting stdout to pipe fd %d\n", exec->fd_pipe[1]);
+		if (dup2(exec->fd_pipe[1], STDOUT_FILENO) == -1)
+		{
+			perror("[ERROR] dup2 failed for pipe output");
+			exit(1);
+		}
+		close(exec->fd_pipe[1]);
+	}
+	printf("[DEBUG] Pipe setup: fd_in=%d, fd_out=%d, p_pipe=%d\n", exec->fd_in, exec->fd_out, exec->p_pipe);
 }
 
-void	apply_redir(t_exec *exec)
+void	apply_redir_pipe(t_exec *exec)
 {
 	if (exec->fd_in != STDIN_FILENO)
 		close(exec->fd_in);
@@ -55,8 +105,8 @@ void	child_process(t_exec *exec, char **envp)
 // 	execute_cmds(exec->cmd_token, envp, exec);
 // 	exit(0);
 	signal(SIGQUIT, SIG_DFL);
-	handle_pipe_redir(exec);
 	setup_redir(exec);
+	handle_pipe_redir(exec);
 	execve(exec->cmd, exec->args, envp);
 	perror("execve");
 	exit(1);
